59d154ea1ef6cbed881e58f59d824478c34d9e1f
Mihail <110977245+mihaildernov@users.noreply.github.com>
2023-10-27 09:52:59 +0500

    Update sbus_receiver.py

--- sbus_receiver.py
+++ sbus_receiver.py
@@ -8,7 +8,6 @@ import time
 class SBUSReceiver():
 	def __init__(self, _uart_port='/dev/ttyS0'):
 
-		#init serial of raspberry pi
 		self.ser = serial.Serial(
 			port=_uart_port,
 			baudrate=115200,
@@ -17,7 +16,6 @@ class SBUSReceiver():
 			bytesize=serial.EIGHTBITS,
 			timeout=0,)
 
-		# constants
 		self.START_BYTE = b'\x0f'
 		self.END_BYTE = b'\x00'
 		self.SBUS_FRAME_LEN = 25
@@ -28,14 +26,12 @@ class SBUSReceiver():
 		self.SBUS_SIGNAL_LOST = 1
 		self.SBUS_SIGNAL_FAILSAFE = 2
 
-		# Stack Variables initialization
 		self.isReady = True
 		self.lastFrameTime = 0
-		self.sbusBuff = bytearray(1)  # single byte used for sync
-		self.sbusFrame = bytearray(25)  # single SBUS Frame
+		self.sbusBuff = bytearray(1) 
+		self.sbusFrame = bytearray(25) 
 		self.sbusChannels = array.array('H', [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])  # RC Channels
 		self.failSafeStatus = self.SBUS_SIGNAL_FAILSAFE
-
 
 	def get_rx_channels(self):
 		"""
@@ -61,9 +57,6 @@ class SBUSReceiver():
 
 	def decode_frame(self):
 
-		# each values are in beetween two or tree differentes bytes . so look the mess to catch it !
-		# Thanks futaba to make this.
-
 		def toInt(_from):
 			return int(codecs.encode(_from, 'hex'), 16)
 
@@ -84,19 +77,16 @@ class SBUSReceiver():
 		self.sbusChannels[14] = ((toInt(self.sbusFrame[20])>>2	|toInt(self.sbusFrame[21])<<6)									& 0x07FF);
 		self.sbusChannels[15] = ((toInt(self.sbusFrame[21])>>5	|toInt(self.sbusFrame[22])<<3)									& 0x07FF);
 
-		#to be tested, No 17 & 18 channel on taranis X8R
 		if toInt(self.sbusFrame[23])  & 0x0001 :
 			self.sbusChannels[16] = 2047
 		else:
 			self.sbusChannels[16] = 0
       
-		#to be tested, No 17 & 18 channel on taranis X8R
 		if (toInt(self.sbusFrame[23]) >> 1) & 0x0001 :
 			self.sbusChannels[17] = 2047
 		else:
 			self.sbusChannels[17] = 0
 
-		#Failsafe
 		self.failSafeStatus = self.SBUS_SIGNAL_OK
 		if toInt(self.sbusFrame[self.SBUS_FRAME_LEN - 2]) & (1 << 2):
 			self.failSafeStatus = self.SBUS_SIGNAL_LOST
@@ -111,47 +101,30 @@ class SBUSReceiver():
 		First find ENDBYTE and looking FRAMELEN backward to see if it's STARTBYTE
 		"""
 
-		#does we have enougth data in the buffer and no thread is currently trying in background?
 		if self.ser.inWaiting() >= self.SBUS_FRAME_LEN*2 and self.isReady:
 			self.isReady = False	
-			#so taking all of them
 			tempFrame = self.ser.read(self.ser.inWaiting())
-			# for each char of the buffer frame we looking for the end byte
+
 			for end in range(0, self.SBUS_FRAME_LEN):
-				#looking for end byte, remember we working backwards
+
 				if tempFrame[len(tempFrame)-1-end] == self.END_BYTE :
-					#looking for start from last hit point minus FRAMELEN
 
 					if tempFrame[len(tempFrame)-end-self.SBUS_FRAME_LEN] == self.START_BYTE :
-						# if it is the right char, frame look good :')
-						# remember data arrive in 8E2 packet so it was already parity verified
-
-						# so the frame have to be remap only if it different (cpu time is precious)
 						lastUpdate = tempFrame[len(tempFrame)-end-self.SBUS_FRAME_LEN:len(tempFrame)-1-end]
+
 						if not self.sbusFrame == lastUpdate:
 							self.sbusFrame = lastUpdate
 							self.decode_frame()
 
-						self.lastFrameTime = time.time() # keep trace of the last update
+						self.lastFrameTime = time.time()
 						self.isReady = True
 						break
 
-
-# excuted if this doc is not imported
-# for testing purpose only
 if __name__ == '__main__':
 
 	sbus = SBUSReceiver('/dev/ttyS0')
 
 	while True:
 		time.sleep(0.005)
-
-		# Call sbus.get_new_data() every about 7 to 10 ms.
-		# to be sure to not calling it to much verify your serial.inWaiting() size of your SBUSReceiver instance.
-		# if beetween call your serial is growing to much (> 50) you can call it more often.
-		# if it raise you < 50 multiples times in row, you calling it too soon.
 		sbus.update()
-
-		# anywere in your code you can call sbus.get_rx_channels() to get all data or sbus.get_rx_channels()[n] to get value of n channel
-		# or get_rx_channel(self, num_ch) to get channel you whant.
 		print(sbus.get_failsafe_status(), sbus.get_rx_channels(), str(sbus.ser.inWaiting()).zfill(4) , (time.time()-sbus.lastFrameTime))
